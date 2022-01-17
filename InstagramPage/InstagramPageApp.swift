5be543c323c4cc55340805b197e40bd1b05d187a
iMtRo <maxmudovilyos75@gmail.com>
2022-01-17 20:09:06 +0500

    Initial Commit

---
+++ InstagramPage/InstagramPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramPageApp.swift
+//  InstagramPage
+//
+//  Created by user on 17/01/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
